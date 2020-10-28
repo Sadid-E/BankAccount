0dcf0163ab8ab5f517b22e60c830c7a857484e17
Sadid-E <sethun20@stuy.edu>
2020-10-27 21:40:00 -0400

    Added instance variables and a constructor

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(double b, int a, String p) {
+    b = balance;
+    a = accountID;
+    p = password;
+  }
+
+}
